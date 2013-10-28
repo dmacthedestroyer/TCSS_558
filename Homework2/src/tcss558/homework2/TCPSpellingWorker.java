0ce6feee503bc416534a67b4d24610528ec19343
Daniel McDonald <mcdonald.daniel.james@gmail.com>
2013-10-27 21:55:41 -0700

    had to add some kind of special session index to the log messages

--- Homework2/src/tcss558/homework2/TCPSpellingWorker.java
+++ Homework2/src/tcss558/homework2/TCPSpellingWorker.java
@@ -9,33 +9,57 @@ import java.net.Socket;
 import java.nio.charset.Charset;
 import java.util.Collection;
 import java.util.StringTokenizer;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * Manages the TCSS Spelling Protocol for a single TCP socket connection to a client.
+ * Manages the TCSS Spelling Protocol for a single TCP socket connection to a
+ * client.
+ * 
  * @author dmac
- *
+ * 
  */
 public class TCPSpellingWorker implements Runnable {
+
+	private static AtomicInteger idCount = new AtomicInteger();
+
+	private int id;
 
 	private final Socket socket;
 
 	private final WordList wordList;
 
+	private void err(String message) {
+		Log.err("" + id + ": " + message);
+	}
+
+	private void out(String message) {
+		Log.out("" + id + ": " + message);
+	}
+
+	private void out(String message, Object... args){
+		Log.out(message, args);
+	}
+	
 	/**
-	 * Creates a new TCPSpellingWorker with the given dictionary and socket connection.
+	 * Creates a new TCPSpellingWorker with the given dictionary and socket
+	 * connection.
+	 * 
 	 * @param wordList
 	 * @param socket
 	 */
 	public TCPSpellingWorker(WordList wordList, Socket socket) {
 		this.wordList = wordList;
 		this.socket = socket;
+		id = idCount.incrementAndGet();
 	}
 
 	/**
-	 * Reads in lines from the client socket as outlined in the TCSS 598 Spelling Protocol, and terminates when the connection is closed.
+	 * Reads in lines from the client socket as outlined in the TCSS 598 Spelling
+	 * Protocol, and terminates when the connection is closed.
 	 */
 	@Override
 	public void run() {
+		Log.out("Connected to client with id ", id);
 		try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 				PrintWriter out = new PrintWriter(new OutputStreamWriter(new BufferedOutputStream(socket.getOutputStream()), Charset.forName("US-ASCII")), true)) {
 			String input;
@@ -47,16 +71,16 @@ public class TCPSpellingWorker implements Runnable {
 					try {
 						id = Integer.parseInt(st.nextToken());
 					} catch (NumberFormatException nfe) {
-						Log.err("Received malformed query (no query number)");
+						err("Received malformed query (no query number)");
 						out.println("INVALID");
 						socket.close();
 						continue;
 					}
 
 					String word = st.nextToken();
-					Log.out(String.format("Query word: %s", word));
+					out("Query word: %s", word);
 					if (wordList.isInList(word)) {
-						Log.out("  Word is spelled correctly");
+						out("  Word is spelled correctly");
 						out.println(String.format("%s OK", id));
 					} else {
 						String logMessage = "  Word is spelled incorrectly, ";
@@ -72,11 +96,11 @@ public class TCPSpellingWorker implements Runnable {
 							logMessage += ".";
 						} else
 							logMessage += "no suggestions.";
-						Log.out(logMessage);
+						out(logMessage);
 						out.println("");
 					}
 				} else {
-					Log.err("Received malformed query (wrong number of arguments)");
+					err("Received malformed query (wrong number of arguments)");
 					out.println("INVALID");
 					socket.close();
 					continue;
@@ -84,7 +108,7 @@ public class TCPSpellingWorker implements Runnable {
 			}
 			out.println("GOODBYE");
 			socket.close();
-			Log.out("Connection closed normally.");
+			out("Connection closed normally.");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
